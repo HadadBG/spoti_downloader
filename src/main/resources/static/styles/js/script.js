094dda2f71adc490d80cb7b226b69d083f9a49d1
Hadad_BG <hadadbautista@gmail.com>
2025-11-14 21:49:33 -0600

    Tabs y usuarios añadidos al modal

--- src/main/resources/static/styles/js/script.js
+++ src/main/resources/static/styles/js/script.js
@@ -3,7 +3,45 @@
     var instances = M.Modal.init(elems);
   });
 
+$(document).ready(function () {
 
+    let step = 1;
+
+    $("#btnSiguiente").click(function () {
+
+        if (step === 1) {
+            $("#paso1").fadeOut(200, function () {
+                $("#paso2").fadeIn(250);
+            });
+
+            $("#btnAtras, #btnCerrar").fadeIn(150);
+            $("#btnSiguiente").css("display","none")
+
+            step = 2;
+        }
+    });
+
+    $("#btnAtras").click(function () {
+
+        if (step === 2) {
+            $("#paso2").fadeOut(200, function () {
+                $("#paso1").fadeIn(250);
+            });
+
+            $("#btnAtras, #btnCerrar").css("display","none");
+            $("#btnSiguiente").fadeIn(150);
+
+            step = 1;
+        }
+
+    });
+
+});
+$(document).on("click", ".copyBtn", function () {
+    let texto = $(this).closest(".copyText").find(".textoCopiable").val();
+    navigator.clipboard.writeText(texto);
+    M.toast({html: 'Copiado!'});
+});
 
 
 $( "#main" ).css( "display", "none" );
@@ -54,7 +92,7 @@ $("#reload_all").on("click",()=>{
 function formate_Date(date){
 
     let  aux= new Date(date);
-    let time = Math.round(new Date()-aux)/1000 ;
+    let time = Math.round(Math.round(new Date()-aux)/1000) ;
       if(time < 60){
         let cad = `hace ${time} segundo`
         if (time > 1){
@@ -212,13 +250,37 @@ async function  loadAsyncData() {
 
 if (!songs || typeof songs === "undefined" || songs == "undefined"){
     songs =clean_data(await getAllSongs())
-    console.log(songs)
+    if (songs.length != 0){
+   
     localStorage.setItem("songs",JSON.stringify(songs))
+    }
 
 }
+ if (songs.length == 0){
+ $( "#main_object" ).html(`
+        <p style="margin-bottom: 1.5rem;"></p>
+        <p>
+                        Bienvenido ${spoti_name}<br/> al parecer no tienes<br/>
+                          canciones
+						<br />
+					</p>
+            <p style="margin-bottom: 2rem;"></p>
+            <p>
+					
+						que te parece si agregas algunas<br/>
+            y vuelves.
+            </p>
+             <p style="margin-bottom: 2rem;"></p>
+            `)
+     $( "#footer_main" ).html(``)
+     return
+ }
       document.getElementById("end_songs").value = songs.length; 
     let less_songs=songs.slice(0,10)
+    if(songs.length >=10){
+      //Agregamos la ultima cancion para saber los indices
         less_songs.push(songs[songs.length - 1])
+    }
        
       
      
@@ -267,7 +329,7 @@ if (!songs || typeof songs === "undefined" || songs == "undefined"){
             <p style="margin-bottom: 2rem;"></p>
             <p>
 					
-						las canciones han sido compiladas
+						las canciones han sido recopiladas
             </p>
              <p style="margin-bottom: 2rem;"></p>
             `)
