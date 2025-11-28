e819b4055466b3b92ad1d47a74c3aae90ed6d235
Hadad Bautista Garcia <hadad.bautista@gmail.com>
2025-11-28 12:14:00 -0600

    Optimized aplication

--- src/main/java/com/example/musicHub/util/WriterThread.java
+++ src/main/java/com/example/musicHub/util/WriterThread.java
@@ -7,48 +7,32 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 
-public class WriterThread extends Thread{
-  File file;
-  ServletOutputStream pos;
-  public WriterThread(File file, ServletOutputStream pos){
-    this.file=file;
-    this.pos=pos;
-  }
-  
-@Override
-    public void run() {
+public class WriterThread extends Thread {
+    private final File file;
+    private final ServletOutputStream pos;
 
-        int BUFF_SIZE = 314572800;//300MB
-                       
-    byte[] buffer = new byte[BUFF_SIZE];
-    int i=1;
-       try {
-           System.out.println(file.toPath());
-           FileInputStream fis =new FileInputStream(file);
-           int len_file=fis.available();
-           int resultado=0;
-           do{
-                System.out.println(BUFF_SIZE*i);
-                if (len_file <  BUFF_SIZE*i){
-                    BUFF_SIZE=(len_file%(BUFF_SIZE*i));
-                    buffer = new byte[BUFF_SIZE];
-                    resultado=-1;
-                }
-   
-              fis.read(buffer,0,BUFF_SIZE);
-              pos.write(buffer);
-                 
-              i+=1;
-           }
-           while(resultado != -1);
-           
-          fis.close();
-          pos.flush();
-      pos.close();
-       }
-       catch (IOException ex) {
-          ex.printStackTrace();
-       }
-       
-  }
+    public WriterThread(File file, ServletOutputStream pos) {
+        this.file = file;
+        this.pos = pos;
+    }
+
+    @Override
+    public void run() {
+        final int BUFFER_SIZE = 8192; // 8KB (ó 16K o 64K)
+        byte[] buffer = new byte[BUFFER_SIZE];
+
+        try (FileInputStream fis = new FileInputStream(file)) {
+
+            int bytesRead;
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                pos.write(buffer, 0, bytesRead);
+            }
+
+            pos.flush();
+            pos.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
