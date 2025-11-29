e5634c5d796b1f4926c89c2e818659fa3af465f9
Hadad Bautista <hadad.bautista@gmail.com>
2025-11-29 11:46:42 -0600

    added configuration for developmente and nginx

--- src/main/java/com/example/musicHub/cotroller/IndexController.java
+++ src/main/java/com/example/musicHub/cotroller/IndexController.java
@@ -30,7 +30,6 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.util.ArrayList;
 import java.util.Base64;
 import java.util.HashMap;
 
